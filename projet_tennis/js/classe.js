705f9a9ebc3a3366a05d8b91bb8134c7f3334736
LouisB10 <113990662+LouisB10@users.noreply.github.com>
2023-04-07 02:19:36 +0200

    Add files via upload

---
+++ projet_tennis/js/classe.js
@@ -0,0 +1,19 @@
+/**********Changement de classe pour les boutons choix de boite ******/
+document.addEventListener("DOMContentLoaded", function() {
+    let boitesChoix = document.querySelectorAll(".boite-choix");
+  
+    boitesChoix.forEach(function(boiteChoix) {
+      boiteChoix.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        // supprimer la classe "active" de toutes les boites-choix
+        boitesChoix.forEach(function(b) {
+          b.classList.remove("active");
+        });
+  
+        // ajouter la classe "active" à la boite-choix cliquée
+        boiteChoix.classList.add("active");
+      });
+    });
+  });
+  
